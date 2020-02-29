88f32909c112be2ace3188d725e4c056b9852112
Gökhan Yener <gkhnyener@gmail.com>
2020-02-29 18:33:23 +0300

    glabal service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout';
-import { FooterComponent } from './layout';
-import { AppLayoutComponent } from './layout';
+import {TopbarComponent} from './layout/partial/topbar/topbar.component';
+import {SubHeaderComponent} from './layout/header/sub-header/sub-header.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import {HomeLayoutComponent} from './layout/app-layout/home-layout/home-layout.component';
+import {SubLayoutComponent} from './layout/app-layout/sub-layout/sub-layout.component';
+import {HeaderComponent} from './layout/header/home/header.component';
+import {ApiService} from './services/api.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopbarComponent,
     HeaderComponent,
-    FooterComponent,
-    AppLayoutComponent,
+    SubHeaderComponent,
+    HomeLayoutComponent,
+    SubLayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
